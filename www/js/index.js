aa334fef08b210af925cfdd6fc4b1164e05cb44e
mantir <mkapp2@gmx.de>
2013-12-17 15:19:19 +0100

    Second commit!

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,7 @@
 var app = {
 	jsonUrl: 'http://headkino.de/potsdamevents/json/',
     backUrl: 'events/index',
+	links : {},
     // Application Constructor
     initialize: function() {
         this.bindEvents();
@@ -14,12 +15,18 @@ var app = {
         this.detailsURL = /events\/view\/(\d+)/
 		//$(window).on('hashchange', $.proxy(this.route, this));
 		$(document).on('tap', 'a[data-href]', app.tapped);
+		$( document ).on( "orientationchange", function( event ) {
+			alert( "This device is in " + event.orientation + " mode!" );
+		});
         $(document).on('pageload', function(){
-                       if(app.afterLoad) {
-                            app.afterLoad();
-                            delete app.afterLoad;
-                       }
-                    });
+			if(app.afterLoad) {
+				app.afterLoad();
+				delete app.afterLoad;
+			}
+		});
+		$(window).on('navigate', function(e, data){
+			console.log(e);
+		});
 		testLive();
         //$('a').on('click', this.go);
         //app.receivedEvent('thelist');
@@ -29,7 +36,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.loadList();
+        Q(app.get('events')).then(function(d){app.render('index', d)});
     },
 	
 	route: function(url, changeHash) {
@@ -40,7 +47,11 @@ var app = {
 			controller = 'events';
 		if(!action)
 			action = 'index';
+		app.links[action] = url;
+		//$.mobile.navigate( action + '.html', { url: url });
 		$.mobile.changePage(action + ".html",{allowSamePageTransition:true,reloadPage:false,changeHash:changeHash,transition:"slide"});
+		
+		this.backUrl = url;
 		app.afterLoad = function(){
 			Q(app.get(url)).then(function(d){app.render(action, d)});
 		}
@@ -55,18 +66,7 @@ var app = {
 		var q = Q.defer();
         $.getJSON(this.jsonUrl + url).done(q.resolve);
 		return q.promise;
-	},
-    // Update DOM on a Received Event
-    loadList: function() {
-        //alert(this.jsonUrl);
-		$.getJSON(this.jsonUrl + 'events', this.renderIndex);
-    },
-                               
-    getEvent: function(id){
-		var q = Q.defer();
-        $.getJSON(this.jsonUrl + 'events/view/' + id).done(q.resolve);
-		return q.promise;
-    },
+	},                        
     
     tapped : function(){
         var url = $(this).data('href');
@@ -74,36 +74,60 @@ var app = {
     },
 	
 	render: function(v, d){
-		v = v[0].toUpperCase() + v.slice(1);
-		app['render' + v ](d);
-		//app['render' + 
+		var V = v[0].toUpperCase() + v.slice(1);
+		if(app['render' + V ])
+			app['render' + V ](d);
+		var $temp = $('#'+ V + '-template'); 
+		if($temp.length) {
+			d = d.vars;
+			console.log(d);
+			var temp = _.template($temp.html());
+			$('.ui-content', '#' + v).html(temp(d));
+		}
+		if(app['after' + V ])
+			app['after' + V ](d);
 	},
 	
 	renderIndex: function(d){
         //alert(5);
-            d = d.vars;
-			//alert(d.events[0].Event.id);
-            var parentElement = document.getElementById('thelist');
-			var place = -1;
-			for(var i in d.events) {
-				var e = d.events[i].Event;
-				var html = '<li><a data-href="events/view/'+ e.id + '">' + e.name + '<br /><span style="font-size:10px">' + e.DateString + '</span></a></li>';
-				if(e.place_id != place) {
-					place = e.place_id;
-					html = '<li data-role="list-divider">' + d.events[i].Place.name + '</li>' + html;
-				}
-				//alert(e.name);
-				//alert(html);
-				$(parentElement).append(html);
-				$(parentElement).listview('refresh');
-			}
+		console.log('renderIndex');
+		d = d.vars;
+		//alert(d.events[0].Event.id);
+		var parentElement = document.getElementById('thelist');
+		var place = -1, epCount = 0, html = '', extra = '';
+		for(var i in d.events) {
+			var e = d.events[i].Event,
+			klasse = 'entry show-all';
+			if(e.place_id != place) {
+				place = e.place_id;
+				epCount = 0;
+				html = '<li class="divider" data-role="list-divider"><a data-href="events/place/'+d.events[i].Place.id+'">' + d.events[i].Place.name + '</a></li>';
+			} else html = '';
+			epCount++;
+			if(epCount < 4) {
+				klasse += ' show-next';
+				extra = '';
+			} else
+				extra = ' style="display:none" ';
+			if(date('d.m.Y') == date('d.m.Y', e.startTime))
+				klasse += ' show-today';
+			html += '<li'+extra+' class="'+klasse+'"><a data-href="events/view/'+ e.id + '">' + e.name + '<br /><span style="font-size:10px">' + e.DateString + '</span></a></li>';
+			//alert(e.name);
+			//alert(html);
+			$(parentElement).append(html);
+			$(parentElement).listview('refresh');
+		}
+		//$(document).list({ headerSelector : 'li.divider' });
         //$('a', parentElement).click(app.tapped);
 	},
 	
-	renderView: function(d){
-		d = d.vars;
-		var e = d.event.Event;
-		$('#headtitle').text(e.name);
+	afterPlace:function(){
+		$('#placeevents').listview();
+	},
+	
+	filterIndex: function(w){
+		$('li.entry').not('.show-'+w).fadeOut(300);
+		$('li.entry.show-'+w).fadeIn(300);
 	}
 };
 
